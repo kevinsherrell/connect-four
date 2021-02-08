c8a668eb2b8c44f5c1280f7bbd0bb08562038dc3
kevinsherrell <k.sherrell.1028@gmail.com>
2021-02-08 09:34:06 -0700

    gameOverModal created/slot selection complete/horizontal check complete

--- app.js
+++ app.js
@@ -9,40 +9,101 @@ class Player {
     };
 };
 
-class Game{
-    constructor(currentPlayer){
+class Game {
+    constructor(currentPlayer) {
         this.currentPlayer = currentPlayer;
         this.gameOver = false;
     }
-    start(){
-        while(this.gameOver === false){
-            console.log("game is working");
-        }
 
+    start() {
     }
-    end(){
+
+    end() {
         this.gameOver = true;
+        gameOverModal.classList.add("true");
     }
-    reset(){}
+
+    reset() {
+    }
 
 }
 
 
-
 let player1 = new Player('Kevin', "red");
 let player2 = new Player('Jonathan', 'black');
+const game = new Game(player1);
 
-let game = new Game(player1);
-
-game.start();
-
+const rows = document.querySelectorAll('.row');
 const gameCell = document.querySelectorAll('.gameCell');
-gameCell.forEach(node =>{
-   node.addEventListener("click", ()=>{
-       console.log("working");
-       node.classList.add("selected");
-       node.classList.add("red");
-   })
+
+const gameOverModal = document.createElement("div");
+gameOverModal.setAttribute('id', 'gameOver');
+document.body.appendChild(gameOverModal);
+// if(game.gameOver === true){
+//     gameOverModal.classList.add('true');
+// }
+const select = (node, currentPlayer) => {
+    currentPlayer = game.currentPlayer;
+    console.log("working");
+    console.log(currentPlayer);
+    if (currentPlayer === player1) {
+        node.classList.add("selected");
+        node.classList.add("red");
+        game.currentPlayer = player2;
+    } else if (currentPlayer === player2) {
+        node.classList.add("selected");
+        node.classList.add("black");
+        game.currentPlayer = player1;
+        console.log(currentPlayer);
+    }
+}
+
+
+const checkHorizontal = () => {
+    let consecutiveRed = 0;
+    let consecutiveBlack = 0;
+
+    // Check horizontally for 3 consecutive chips following current chip (4 consecutive chips);
+    for (let i = 0; i < 41; i++) {
+        if (gameCell[i].classList.contains('red') && gameCell[i + 1].classList.contains('red')) {
+            consecutiveRed++;
+        } else if (gameCell[i].classList.contains('black') && gameCell[i + 1].classList.contains('black')) {
+            consecutiveBlack++;
+        }
+
+    }
+    if (consecutiveRed === 3) {
+        console.log("player 1 wins!");
+        game.end()
+    } else if (consecutiveBlack === 3) {
+        console.log("player 2 wins");
+        game.end();
+    }
+    console.log("consecutive black", consecutiveBlack);
+    console.log("consecutive red", consecutiveRed);
+}
+// console.log(rows);
+gameCell.forEach((node, index) => {
+
+    node.addEventListener('click', () => {
+
+        console.log(index);
+        console.log(gameCell[index]);
+
+        /* This controls the basic mechanics of the game.
+           The bottom rown can always be selected.
+           any cells in row other than the bottom can only be selected if there is already a chip in the previous spot in the previous row.
+        */
+        if (index >= 35 && index <= 41 || gameCell[index + 7].classList.contains("selected")) {
+            select(node);
+            checkHorizontal();
+        }
+
+
+        // Check vertically for 3 consecutive chips following current chip (4 consecutive);
+        // Check diagonally for 3 consecutive chips following current chip (4 consecutive);
+
+    })
 })
-console.log(gameCell.length)
+// console.log(gameCell.length)
 
