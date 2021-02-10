8221b0a0c9581584a0f39d4f4396c33624c22d8e
kevinsherrell <k.sherrell.1028@gmail.com>
2021-02-10 15:22:10 -0700

    moved element back to classes

--- app.js
+++ app.js
@@ -10,90 +10,25 @@ for example:
 
 // Classes
 class Game {
-    currentPlayer = 'player 1';
-    gameOver = true;
-    winner = "";
-    grid = [
-        [-1, -1, -1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, -1, -1, -1]
-    ];
-    cells = document.querySelectorAll('.column');
-
-    startModal = document.createElement('div')
-
-    init() {
-        /*
-        This modal will be displayed before anything else on the screen. Game content only displays when game is started;
-    * */
-        this.startModal.classList.add('gameStart');
-        this.startModal.classList.add('true');
-        this.startModal.innerHTML = `
-            <img src="./images/header_logo.png" alt="">
-            <input type="text" class="nameInput">
-            <button class="colorInput">Red</button>
-            <button class="colorInput">Black</button>
-            <button class="startGame">Start Game!</button>
-`
-        document.body.appendChild(this.startModal);
-        document.querySelector('.startGame').addEventListener('click', (e) => {
-            game.start();
-        })
-        console.log(this.startModal);
+    constructor() {
+        this.currentPlayer = {};
+        this.gameOver = true;
+        this.winner = "";
+        this.grid = [
+            [-1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1]
+        ];
+        this.gameBoard = document.createElement('div');
     }
 
+
     start() {
-        // game over becomes false to display the gameBoard
         this.gameOver = false;
-        // get rid of start modal
-        this.startModal.classList.remove("true");
-
-        // generate game board
-        const playArea = document.createElement('div');
-        playArea.setAttribute('id', 'playArea');
-        playArea.className = this.gameOver === true ? 'gameOver' : null;
-
-        const gameBoard = document.createElement('div');
-        gameBoard.setAttribute('id', 'gameBoard');
-
-        for (let i = 0; i < this.grid.length; i++) {
-            let row = document.createElement("div");
-            row.classList.add("row")
-            row.setAttribute("data-row", `${i}`)
-            for (let j = 0; j < this.grid[i].length; j++) {
-                let column = document.createElement("div");
-                column.classList.add('column');
-                column.setAttribute('data-row', `${i}`);
-                column.setAttribute('data-column', `${j}`);
-                row.appendChild(column);
-                console.log("working");
-            }
-            gameBoard.appendChild(row);
-        }
-        // append gameboard to play area and play area to body
-        playArea.appendChild(gameBoard);
-        document.body.appendChild(playArea);
-
-        // add event listener to each cell
-        this.cells.forEach((cell) => {
-            cell.addEventListener('click', (e) => {
-                let row = parseInt(cell.dataset.row);
-                let column = parseInt(cell.dataset.column);
-                /*
-                 selection conditions (cell is selectable if....):
-                    selection is on bottom row (5) or...
-                    the cell in the row below current row is selected.
-                */
-                if (cell.dataset.row == 5 || game.grid[row + 1][column] != -1) {
-                    game.grid[row][column] = 'selected';
-                    console.log(this.grid);
-                }
-            })
-        })
-
+        this.gameBoard.classList.toggle("gameOver");
 
     }
 
@@ -102,12 +37,64 @@ class Game {
     };
 
     reset() {
-
     }
+
+    generateBoard() {
+        // generate game board/play area
+        this.gameBoard.setAttribute('id', 'gameBoard');
+        this.gameBoard.classList.add("gameOver");
+        for (let i = 0; i < game.grid.length; i++) {
+            let row = document.createElement("div");
+            row.classList.add("row")
+            row.setAttribute("data-row", `${i}`)
+            for (let j = 0; j < game.grid[i].length; j++) {
+                let column = document.createElement("div");
+                column.classList.add('column');
+                column.setAttribute('data-row', `${i}`);
+                column.setAttribute('data-column', `${j}`);
+                row.appendChild(column);
+                console.log("working");
+            }
+            this.gameBoard.appendChild(row);
+        }
+        //===================================================================
+
+        // append gameboard to play area and play area to body
+        document.body.appendChild(this.gameBoard);
+        // document.body.appendChild(playArea);
+        //====================================================================
+
+        // add event listener to each cell
+        const cells = document.querySelectorAll('.column');
+        cells.forEach((cell) => {
+            cell.addEventListener('click', (e) => {
+                let row = parseInt(cell.dataset.row);
+                let column = parseInt(cell.dataset.column);
+                game.currentPlayer.select(cell, row, column);
+            })
+        })
+        //============================================================
+    }
+
+    generateStartModal() {
+        let startButton = document.querySelector('.startButton');
+        console.log(startButton);
+        startButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            game.start();
+            console.log('click working')
+        })
+//============================================================
+    }
+
+    check
+
+    init() {
+    }
+
 }
 
 
-// Player
 class Player {
     constructor(name, color, wins, losses) {
         this.name = name;
@@ -116,13 +103,39 @@ class Player {
         this.losses = losses;
     };
 
-    select() {
-
+    select(element, row, column) {
+        /*
+         selection conditions (cell is selectable if....):
+            selection is on bottom row (5) or...
+            the cell in the row below current row is selected.
+        */
+        if (element.dataset.row == 5 || game.grid[row + 1][column] != -1) {
+            game.grid[row][column] = 'selected';
+            element.classList.add("selected")
+            element.classList.add(game.currentPlayer.color);
+            if (game.currentPlayer === player1) {
+                game.currentPlayer = player2;
+            } else {
+                game.currentPlayer = player1;
+            }
+            console.log(game.grid);
+            console.log(element)
+        }
     }
 };
 
+// Create players
+const player1 = new Player('Kevin', 'red');
+const player2 = new Player('cpu', 'black');
+// Create game object
 const game = new Game();
-const player1 = new Player();
+game.currentPlayer = player1; // player1 always goes first.
+game.generateStartModal();
+game.generateBoard();
 
-game.init();
-
+/*
+Horizontal check logic:
+    if row at index is equal to row at index + 1 add 1 to count
+        if count equals 3 current player wins
+            if row at index is not equal to row at index + 1 count is reset to zero;
+ */
